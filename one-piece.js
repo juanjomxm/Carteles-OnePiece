1c3a55e90d3a2e8e21d2bf4a5cfbdbc3b87c2462
juanjo314 <juanchi_z_29@hotmail.com>
2023-04-16 15:34:38 -0500

    mas carteles con boton recompensa

--- one-piece.js
+++ one-piece.js
@@ -1,13 +1,25 @@
-const botonUsopp = document.querySelector("#boton-usopp")
-botonUsopp.addEventListener("click", usopp)
+const botonRecompensa = document.querySelector("#boton-recompensa")
+botonRecompensa.addEventListener("click", recompensa1)
 
-function usopp(){
+function recompensa1(){
     const p1 = document.querySelector("#r1")
     const p2 = document.querySelector("#r2")
 
-    if(botonUsopp){
-        p1.innerHTML = 500000
-        p2.innerHTML = 30000
-        return
+    if(botonRecompensa){
+        p1.innerHTML = "$" + 200000000 + " BERRYS"
+        p2.innerHTML = "$" + 100000000 + " BERRYS"
+    }
+}
+
+const botonRecompensa2 = document.querySelector("#boton-recompensa2")
+botonRecompensa2.addEventListener("click", recompensa2)
+
+function recompensa2(){
+    const p3 = document.querySelector("#r3")
+    const p4 = document.querySelector("#r4")
+
+    if(botonRecompensa2){
+        p3.innerHTML = "$" + 83000000 + " BERRYS"
+        p4.innerHTML = "$" + 100000000 + " BERRYS"
     }
 }
